36e45aa3e0fcf6422046ea29a4aaf824e147f907
Jayant <jayant.kizhepatt@honeywell.com>
2016-10-08 14:49:59 +0530

    External Configuration Manager

---
+++ ConfigManager/ConfigManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+
+namespace ConfigManager
+{
+    public interface IConfigManager : IService
+    {
+        Task<string> GetApplicationConfiguration(string key);
+    }
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class ConfigManager : StatelessService, IConfigManager
+    {
+        public async Task<int> AddNewApplicationConfigurationValue(string partition, string key, string value)
+        {
+            //
+            
+            return 1;
+        }  
+        public Task<string> GetApplicationConfiguration(string key)
+        {
+            //TODO: Get the configuration from the cache
+            return Task.FromResult("dummy-string");
+        }
+        public ConfigManager(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new[] { new ServiceInstanceListener(this.CreateServiceRemotingListener) };
+           // return new ServiceInstanceListener[0];
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following sample code with your own logic 
+            //       or remove this RunAsync override if it's not needed in your service.
+
+           
+        }
+    }
+}
---
+++ ConfigMgmtSvc/Controllers/ConfigController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using ConfigManager;
+
+namespace ConfigMgmtSvc.Controllers
+{
+    public class ConfigController : ApiController
+    {
+        
+        public IEnumerable<string> Get(string key)
+
+        {
+            return new string[] { "value1asas", "value2" };
+        }
+
+        
+
+
+        // POST api/values 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+
+        
+        [Route("api/config/{appKey}/get")]
+        [HttpGet]
+        public async Task<IHttpActionResult> getconfig([FromUri] string appKey)
+        {
+            /*
+             * 
+             * IMyService helloWorldClient = ServiceProxy.Create<IMyService>(new Uri("fabric:/MyApplication/MyHelloWorldService"));
+
+                string message = await helloWorldClient.GetHelloWorld();
+             */
+            try
+            {
+                var config =
+                    ServiceProxy.Create<IConfigManager>(
+                        new Uri("fabric:/ConfigurationManagementService/ConfigManager"));
+                var result = await config.GetApplicationConfiguration(appKey);
+                return Ok(result);
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }  
+            return Ok();
+        }
+    }
+}
